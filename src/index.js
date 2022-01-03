203939b6d7f40aca2b0a04cd6ec85a4a5916543d
SmSka2021 <91590651+SmSka2021@users.noreply.github.com>
2022-01-03 23:15:28 +0300

    conplete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    while (n % 10 === 0){
+        n = Math.trunc(n / 10);
+        }
+        let numberStr = Math.abs(n).toString();
+        let arr = numberStr.split("").reverse().join("")
+        return +arr;
 }
